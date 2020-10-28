f956aad6725c29424f8d4b1d123450b71f4b06cd
Matt <mwatt0204@gmail.com>
2020-10-28 10:39:24 +0000

    DFID working

--- main.py
+++ main.py
@@ -2,19 +2,14 @@
 
 import sys
 import copy
-import  time
+import time
+import csv
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-timesran = 0
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+timesran = 0  # global variable for tracking number of moves
 
 
 def move_blank(i, j, n):
@@ -68,16 +63,15 @@ def dfd(path, maxDepth):
         return None
 
     else:
-        laststate = copy.deepcopy(path[-1])  #deep copy as move method yeilds
+        laststate = copy.deepcopy(path[-1])  # deep copy as move method yeilds
         for nextState in move(laststate):
-            timesran = timesran + 1  ## count nubmer of moves made
+            timesran = timesran + 1  # count nubmer of moves made
             if nextState not in path:
                 nextPath = path + [nextState]  # adds iteam to array at end
                 solution = dfd(nextPath, maxDepth - 1)
                 if solution is not None:  # maybe this line
                     return solution
     return None
-
 
 
 def DFIDWithTmingsandMovesandLengths(state):
@@ -87,8 +81,8 @@ def DFIDWithTmingsandMovesandLengths(state):
     startTime = time.time()
     soloutin = DFID(path)
     endTime = time.time()
-    runtime = endTime-startTime
-    return soloutin,timesran,runtime
+    runtime = endTime - startTime
+    return soloutin, timesran, runtime
 
 
 def stateToString(state):
@@ -100,31 +94,26 @@ def stateToString(state):
     print('\n')
 
 
-
-def runFromCSV(file):
-
-    sloution,moves,time = DFIDWithTmingsandMovesandLengths(state)
-    print(len(sloution)-1,":Shortest number of Moves")
-    print(moves , "moves in path")
-    print(time, "seconds")
-
-
-
-
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-    state = [0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]]
+    #array of states given in brefi
+    startStates = [
+        [0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
+        [0, 2, [[5, 6, 0], [1, 3, 8], [4, 7, 2]]],
+        [2, 0, [[3, 5, 6], [1, 2, 7], [0, 8, 4]]],
+        [1, 1, [[7, 3, 5], [4, 0, 2], [8, 1, 6]]],
+        [2, 0, [[6, 4, 8], [7, 1, 3], [0, 2, 5]]],
+        [0, 2, [[3, 2, 0], [6, 1, 8], [4, 7, 5]]],
+        [0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
+        [2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
+        [0, 0, [[0, 7, 1], [5, 4, 8], [6, 2, 3]]],
+        [0, 2, [[5, 4, 0], [2, 3, 1], [8, 7, 6]]],
+        [2, 1, [[8, 6, 7], [2, 5, 4], [3, 0, 1]]]
+    ]
 
-#    path = [state]
- #   print(DFID(path))
-    # print(dfs(path, 16))
-  #  print(timesran)
-
-    sloution,moves,time = DFIDWithTmingsandMovesandLengths(state)
-    print(len(sloution)-1,":Shortest number of Moves")
-    print(moves , "moves in path")
-    print(time, "seconds")
+    for state in startStates:
+        sloution, moves, timetaken = DFIDWithTmingsandMovesandLengths(state)
+        print(len(sloution) - 1, ":Shortest number of Moves")
+        print(moves, "moves in path")
+        print(timetaken, "seconds")
